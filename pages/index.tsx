8e27e19a47b1a08dc27daf9b5a4f0e96f90ffbf9
siyeon0 <siyeon.kang1@gmail.com>
2022-11-08 19:43:36 +0000

    fixed loading error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "@chakra-ui/react"
+import { Stack, Wrap, WrapItem } from "@chakra-ui/react"
 import { NextPage } from "next"
 import Container from "../components/container"
+import GdscCanvas from "../components/gdsc-canvas"
 import Intro from "../components/intro"
 
 const Home: NextPage = () => {
@@ -13,6 +14,12 @@ const Home: NextPage = () => {
         alignItems="flex-start"
         px={{base: '5vw', md: '7.5vw'}}
         mt={{base: '12.5vh', md: '22.5vh'}}>
+        <Wrap>
+          <WrapItem><Intro /></WrapItem>
+          <WrapItem><GdscCanvas /></WrapItem>
+
+        </Wrap>
+        
         </Stack>       
       </Container>
     </>
